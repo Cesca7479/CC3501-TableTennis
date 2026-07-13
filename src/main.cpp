d2c7b3bb0d4676303f0d9bc630b3f6a360b48197
jd117390 <francesca.adcock@my.jcu.edu.au>
2026-07-13 21:31:15 +1000

    Implement led display functionality

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,56 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
+#include "hardware/i2c.h"
 
 #include "WS2812.pio.h"
 #include "drivers/logging/logging.h"
 
 #define LED_PIN 14
+
+// For the led display
+#define I2C_PORT i2c0
+#define SCL_PIN 17
+#define SDA_PIN 16
+#define HT16K33_ADDR 0x70
+#define OSCILLATOR_ON 0x21
+#define DISPLAY_ON 0x81
+#define BRIGHTNESS 255
+const uint8_t digits[] =
+    {
+        0b00111111, // 0
+        0b00000110, // 1
+        0b01011011, // 2
+        0b01001111, // 3
+        0b01100110, // 4
+        0b01101101, // 5
+        0b01111101, // 6
+        0b00000111, // 7
+        0b01111111, // 8
+        0b01101111  // 9
+};
+
+void ht16k33_display_number(int value, uint8_t decimal_mask = 0)
+{
+    uint8_t tx[9];
+    tx[0] = 0;
+    static const uint16_t divisors[] = {1000, 100, 10, 1};
+
+    for (int digit = 0; digit < 4; digit++)
+    {
+        uint8_t segments = digits[(value / divisors[digit]) % 10];
+
+        // Turn on decimal point if requested
+        if (decimal_mask & (1 << (3 - digit)))
+        {
+            segments |= 0x80;
+        }
+        tx[1 + digit * 2] = segments;
+        tx[2 + digit * 2] = 0;
+    }
+
+    i2c_write_blocking(I2C_PORT, HT16K33_ADDR, tx, sizeof(tx), false); // sizeof(tx) = 9 for all lit up. Change this later if don't want
+}
 
 int main()
 {
@@ -17,23 +62,33 @@ int main()
     ws2812_program_init(pio0, 0, pio_program_offset, LED_PIN, 800000, false);
     uint32_t led_data[1];
 
+    // Set up led display
+    i2c_init(I2C_PORT, 400 * 1000); // Set the clock (400kHz)
+    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(SDA_PIN);
+    gpio_pull_up(SCL_PIN); // May not need these... only if forgot resistors??
+
+    uint8_t cmd;
+
+    cmd = OSCILLATOR_ON;
+    i2c_write_blocking(I2C_PORT, HT16K33_ADDR, &cmd, 1, false);
+
+    cmd = DISPLAY_ON;
+    i2c_write_blocking(I2C_PORT, HT16K33_ADDR, &cmd, 1, false);
+
+    cmd = BRIGHTNESS;
+    i2c_write_blocking(I2C_PORT, HT16K33_ADDR, &cmd, 1, false);
+
     for (;;)
     {
         // Test the log system
         log(LogLevel::INFORMATION, "Hello world");
-
-        // Turn on the first LED to be a certain colour
-        uint8_t red = 0;
-        uint8_t green = 0;
-        uint8_t blue = 255;
-        led_data[0] = (red << 24) | (green << 16) | (blue << 8);
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
-        sleep_ms(500);
-
-        // Set the first LED off
-        led_data[0] = 0;
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
-        sleep_ms(500);
+        for (uint16_t i = 0; i <= 9999; i++)
+        {
+            ht16k33_display_number(i, i % 16);
+            sleep_ms(100);
+        }
     }
 
     return 0;
