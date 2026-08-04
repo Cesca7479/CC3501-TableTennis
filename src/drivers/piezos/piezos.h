aefd0ad1822934916e5cadd6be9a9f9264ee41d6
jd117390 <francesca.adcock@my.jcu.edu.au>
2026-08-05 03:04:40 +1000

    Add and test sounds

--- src/drivers/piezos/piezos.h
+++ src/drivers/piezos/piezos.h
@@ -7,31 +7,36 @@
 #include "drivers/logging/logging.h"
 #include "stdio.h"
 
+class Piezo
+{
 
-class Piezo {
+private:
+    uint16_t raw_result;
+    int ADC_input;
+    int num_bounces = 0;
+    void adc_irq_handler();
+    uint slice_num;
+    int GPIO_sensing_pin;
+    int GPIO_buzzer_pin;
+    void init_buzzer();
+    void stop_buzzer();
 
-    private:
-        uint16_t raw_result;
-        int ADC_input;
-        int num_bounces = 0;
-        void adc_irq_handler();
-        void init_buzzer();
-        void stop_buzzer();
-        uint slice_num;
-        int GPIO_sensing_pin;
-        int GPIO_buzzer_pin;
-        
-    public:
-        bool buzzer_on = false;
-        void init_sensing();
-        void play_tone(uint32_t frequency);
-        void play_victory_sequence();
-        uint16_t read();
-        Piezo(int sensing_pin, int buzzer_pin) {
-            GPIO_sensing_pin = sensing_pin;
-            GPIO_buzzer_pin = buzzer_pin;
-            ADC_input = GPIO_sensing_pin - 26;
-        };
+public:
+    bool buzzer_on = false;
+    void init_sensing();
+    void play_tone(uint32_t frequency);
+    void play_victory_sequence();
+    void play_angry_sounds();
+    void play_point();
+    void play_serve();
+    void play_select();
+    uint16_t read();
+    Piezo(int sensing_pin, int buzzer_pin)
+    {
+        GPIO_sensing_pin = sensing_pin;
+        GPIO_buzzer_pin = buzzer_pin;
+        ADC_input = GPIO_sensing_pin - 26;
+    };
 };
 
 extern Piezo Piezos[3];
