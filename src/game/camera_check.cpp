f8647f227e2e1828bbfa158e7d84a05208821473
Jamie7846 <96859115+Jamie7846@users.noreply.github.com>
2026-08-04 14:42:10 +1000

    Changed gamestate to allow camera check mode to update it

--- src/game/camera_check.cpp
+++ src/game/camera_check.cpp
@@ -4,6 +4,8 @@ void run_camera_check_mode()
 {
     // Cesca do your stuff to check bluetooth or call the function from your driver
 
-    // State.ball_location = BALL_LEFT, BALL_CENTER_LEFT, BALL_CENTER_RIGHT, BALL_RIGHT <-- these already defined in enum in gamestate
-    
+    // if it returns that location has changed change:
+    // State.ball_location = PLAYER_1 or PLAYER_2 (PLAYER_1 should be on the left facing our robot guy, left or right depends on where you want to setup the rpi)
+    // State.ball_is_center = bool
+    State.mode = State.camera_check_return_to_mode; // State changes back to either serve detection mode, or bounce listening mode
 }
--- src/game/gamestate.h
+++ src/game/gamestate.h
@@ -6,12 +6,9 @@
 #include "drivers/piezos/piezos.h"
 
 
-
-
-
 #define SENSITIVITY_THRESHOLD_TABLE 80
 #define SENSITIVITY_THRESHOLD_NET 80
-#define BOUNCE_SAMPLING_RATE_MS 100 // Sample once every 100ms to prevent overcounting bounces
+#define BOUNCE_SAMPLING_RATE_MS 100 // Sample max once every 100ms per sensor to prevent overcounting bounces
 #define TIME_OUT_THRESHOLD_MS 2000
 
 enum State
@@ -25,23 +22,25 @@ enum State
     FOUL  
 };
 
-enum Location {
-    BALL_LEFT,
-    BALL_CENTER_LEFT,
-    BALL_CENTER_RIGHT,
-    BALL_RIGHT
+enum Bounce_Location
+{
+    PLAYER_1,
+    PLAYER_2,
+    NET
 };
 
 
 struct GameState
 {
     uint8_t mode = SETUP_GAME;
+    uint8_t camera_check_return_to_mode = SERVE_DETECTION;
     GameMode game_mode = GameMode::NO_MODE_SELECTED;
     uint8_t game_number = 0;
     uint8_t player_score[2] = {0, 0};
     uint8_t player_serving = PLAYER_1;
     uint8_t prev_bounce_side;
     uint8_t ball_location = PLAYER_1;
+    bool ball_is_center = false;
     absolute_time_t prev_bounce_time;
     absolute_time_t last_read_time[3] = {0,0,0};
     GameSettings settings = GameSettings{};
