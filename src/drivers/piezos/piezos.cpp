88a532ccc8d86bb9ee277ff4f0e0450f54719e1d
jd117390 <francesca.adcock@my.jcu.edu.au>
2026-08-05 00:52:18 +1000

    Set up music testing

--- src/drivers/piezos/piezos.cpp
+++ src/drivers/piezos/piezos.cpp
@@ -1,41 +1,45 @@
 #include "piezos.h"
 
-
 Piezo Piezos[3] = {
     Piezo(VIBRATION_OUTPUT1_PIN, BUZZER1_PIN),
     Piezo(VIBRATION_OUTPUT2_PIN, BUZZER2_PIN),
-    Piezo(VIBRATION_OUTPUT3_PIN, BUZZER3_PIN)
-};
+    Piezo(VIBRATION_OUTPUT3_PIN, BUZZER3_PIN)};
 
+const uint32_t notes5[7] = {523, 587, 659, 698, 784, 880, 988};
+const uint32_t notes6[7] = {1047, 1175, 1319, 1397, 1568, 1760, 1976};
+const uint32_t notes7[7] = {2093, 2349, 2637, 2794, 3136, 3520, 3951};
 
-const uint32_t notes[6] = {500, 600, 800, 1200, 1300, 1300};
-
-void Piezo::init_sensing() {
+void Piezo::init_sensing()
+{
     adc_init();
     adc_gpio_init(GPIO_sensing_pin);
 }
 
-uint16_t Piezo::read() {
+uint16_t Piezo::read()
+{
     adc_select_input(ADC_input);
     raw_result = adc_read();
     return raw_result;
 }
 
-void Piezo::init_buzzer() {
+void Piezo::init_buzzer()
+{
     buzzer_on = true;
     gpio_set_function(GPIO_buzzer_pin, GPIO_FUNC_PWM);
     slice_num = pwm_gpio_to_slice_num(GPIO_buzzer_pin);
     pwm_set_clkdiv(slice_num, 4.0f);
 }
 
-void Piezo::play_tone(uint32_t frequency) {
-    uint32_t wrap = 125000000 / (4 * frequency) -1;
+void Piezo::play_tone(uint32_t frequency)
+{
+    uint32_t wrap = 125000000 / (4 * frequency) - 1;
     pwm_set_wrap(slice_num, wrap);
     pwm_set_gpio_level(GPIO_buzzer_pin, wrap / 2);
     pwm_set_enabled(slice_num, true);
 }
 
-void Piezo::stop_buzzer() {
+void Piezo::stop_buzzer()
+{
     pwm_set_enabled(slice_num, false);
     gpio_set_function(GPIO_buzzer_pin, GPIO_FUNC_SIO);
     gpio_set_dir(GPIO_buzzer_pin, GPIO_OUT);
@@ -43,19 +47,20 @@ void Piezo::stop_buzzer() {
     sleep_ms(1000);
     gpio_set_dir(GPIO_buzzer_pin, GPIO_IN);
     gpio_disable_pulls(GPIO_buzzer_pin);
-    
+
     // sleep_ms(1000);
     // init_sensing();
     // sleep_ms(1000);
     buzzer_on = false;
 }
 
-void Piezo::play_victory_sequence() {
+void Piezo::play_victory_sequence()
+{
     init_buzzer();
 
-    for (size_t i = 0; i < 6; i++)
+    for (size_t i = 0; i < 7; i++)
     {
-        play_tone(notes[i]);
+        play_tone(notes5[i]);
         sleep_ms(50);
     }
     stop_buzzer();
