97d2b49cbac088f61ba1b74656ceb78e83901736
jd119630 <keya.wong@my.jcu.edu.au>
2026-08-07 10:41:10 +1000

    Remove wait_for_select_release from change_score.cpp

--- src/game/change_score.cpp
+++ src/game/change_score.cpp
@@ -18,21 +18,7 @@ enum class ButtonControllerMode
     SCORE_EDIT_VALUE
 };
 
-static void wait_for_select_release()
-{
-    // Clear the event that opened this mode
-    (void)is_button_pressed(SELECT_BUTTON);
-
-    // Do not continue until the physical button is released
-    while (is_button_on(SELECT_BUTTON))
-    {
-        sleep_ms(1);
-    }
-
-    // Clear any bounce event produced during release
-    (void)is_button_pressed(SELECT_BUTTON);
-}
-
+// Show score if visibility is enabled
 static void show_score_selection(bool visible)
 {
     display_player_score(
