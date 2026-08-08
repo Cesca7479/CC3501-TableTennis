0034f0579e2f5d73e484ef3a60f8031a8206ba0f
Jamie7846 <96859115+Jamie7846@users.noreply.github.com>
2026-08-08 19:25:25 +1000

    Fix #include errors

--- src/game/setup_game/setup_game.h
+++ src/game/setup_game/setup_game.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "gamestate.h"
+#include "game/gamestate.h"
 #include "drivers/hat_id/hat_id.h"
 #include "drivers/logging/logging.h"
 #include "drivers/display/display.h"
--- src/game/setup_round/setup_round.h
+++ src/game/setup_round/setup_round.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "gamestate.h"
+#include "game/gamestate.h"
 #include "stdio.h"
 #include "pico/stdlib.h"
 
